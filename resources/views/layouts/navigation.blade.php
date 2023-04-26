c47f2668fb662339715969f70ecf0f539c664488
Oscar Navarrete <oscarit@yascemail.com>
2023-04-26 13:05:50 -0600

    Welcome design

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <img src="{{ asset('images/identity/smart_blog_icon.png') }}" class="navbar_icon" alt="Smart blog logo">
                     </a>
                 </div>
 
@@ -14,6 +14,9 @@
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('blogs')">
+                        {{ __('Blogs') }}
                     </x-nav-link>
                 </div>
             </div>
@@ -45,7 +48,7 @@
                             <x-dropdown-link :href="route('logout')"
                                     onclick="event.preventDefault();
                                                 this.closest('form').submit();">
-                                {{ __('Log Out') }}
+                                {{ __('Cerra sesión') }}
                             </x-dropdown-link>
                         </form>
                     </x-slot>
